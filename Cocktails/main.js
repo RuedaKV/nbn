b45e98b9f8fda6f35ff51237a02c27770040cb21
Kevin Rueda <65484407+RuedaKV@users.noreply.github.com>
2021-07-20 23:49:50 -0500

    Add files via upload

---
+++ Cocktails/main.js
@@ -0,0 +1,63 @@
+// NAV BAR SCROLL EFFECT
+
+const navColorChange = () => {
+    document.querySelector('nav').className = "nav-bar nav-bar-scroll";
+}
+
+window.addEventListener("scroll", navColorChange);
+
+// ADD ANY OTHER JS BELOW:
+
+function weinberg() {
+    var x = document.getElementById("weinberg-div");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function comms() {
+    var x = document.getElementById("comms-div");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function bienin() {
+var x = document.getElementById("bienin-div");
+if (x.style.display === "none") {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+}
+
+function mccormick() {
+    var x = document.getElementById("mccormick-div");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    }
+
+function sesp() {
+    var x = document.getElementById("sesp-div");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    }
+
+function medill() {
+    var x = document.getElementById("medill-div");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    }
